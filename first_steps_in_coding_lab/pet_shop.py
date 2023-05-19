c452ce203c145d460a8273475d2fc1eb9c738573
Petar-Koprinkov <134071867+Petar-Koprinkov@users.noreply.github.com>
2023-05-19 17:11:53 +0300

    Add files via upload

---
+++ first_steps_in_coding_lab/pet_shop.py
@@ -0,0 +1,7 @@
+number_of_package_for_dogs = int(input())
+number_of_package_for_cats = int(input())
+price_of_the_food_for_dogs = number_of_package_for_dogs * 2.50
+price_of_the_food_for_cats = number_of_package_for_cats * 4
+total_price = price_of_the_food_for_dogs + price_of_the_food_for_cats
+
+print(f"{total_price} lv.")
